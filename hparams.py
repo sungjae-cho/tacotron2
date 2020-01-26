801df9c2063d59fc62f34efcb2b27e0d6c8c9c07
Sungjae Cho <sungjae.cho.1118@gmail.com>
2020-01-26 23:24:08 +0900

    use_saved_learning_rate=True

--- hparams.py
+++ hparams.py
@@ -112,7 +112,7 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Optimization Hyperparameters #
         ################################
-        use_saved_learning_rate=False,
+        use_saved_learning_rate=True,
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0, # gradient clipping L2-norm
